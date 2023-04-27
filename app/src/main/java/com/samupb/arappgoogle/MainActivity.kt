fe6c8a8b33b16dc7da8552436e0981a95ffa90a0
samuelipd <>
2023-04-27 17:10:11 -0500

    example with ArSceneView

--- app/src/main/java/com/samupb/arappgoogle/MainActivity.kt
+++ app/src/main/java/com/samupb/arappgoogle/MainActivity.kt
@@ -1,51 +1,95 @@
 package com.samupb.arappgoogle
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import com.google.ar.core.Anchor
-import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.TransformableNode
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.getDescription
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.math.Position
+
+data class Model(
+    val fileLocation: String,
+    val scaleUnits: Float? = null,
+    val placementMode: PlacementMode = PlacementMode.BEST_AVAILABLE,
+    val applyPoseRotation: Boolean = true
+)
 
 class MainActivity : AppCompatActivity() {
 
-    var arFragment = ArFragment()
+    private lateinit var sceneView: ArSceneView
+    lateinit var loadingView: View
+    lateinit var statusText: TextView
     var modelAdded = false
+    var isLoading = false
+        set(value) {
+            field = value
+            loadingView.isGone = !value
+        }
+
+    var modelNode: ArModelNode? = null
+
+    val model = Model("converser.glb")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
-        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-            val anchor = hitResult.createAnchor()
-            ModelRenderable.builder()
-                .setSource(this, Uri.parse("wolves.sfb"))
-                .build()
-                .thenAccept { modelRenderable ->
-                    if (!modelAdded) {
-                        addModelToScene(anchor, modelRenderable)
-                        modelAdded = true
-                    }
-                }
-                .exceptionally { throwable ->
-                    println("errorAr: ${throwable.message}")
-                    val toast = android.widget.Toast.makeText(this, "Error", android.widget.Toast.LENGTH_SHORT)
-                    toast.show()
-                    null
-                }
+
+        statusText = findViewById(R.id.statusText)
+        sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
+            onArTrackingFailureChanged = { reason ->
+                statusText.text = reason?.getDescription(context)
+                // statusText.isGone = reason == null
+            }
+        }
+        loadingView = findViewById(R.id.loadingView)
+        sceneView.onTapAr = { hitResult, motionEvent ->
+            if (!modelAdded) {
+                newModelNode()
+                placeModelNode()
+            }
         }
     }
 
-    private fun addModelToScene(anchor: Anchor?, modelRenderable: ModelRenderable?) {
-        val anchorNode = AnchorNode(anchor)
-        val transformableNode = TransformableNode(arFragment.transformationSystem)
-        transformableNode.setParent(anchorNode)
-        transformableNode.renderable = modelRenderable
-        arFragment.arSceneView.scene.addChild(anchorNode)
-        transformableNode.scaleController.minScale = 0.1f
-        transformableNode.scaleController.maxScale = 0.5f
-        transformableNode.select()
+    fun placeModelNode() {
+        modelNode?.anchor()
+        sceneView.planeRenderer.isVisible = false
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun newModelNode() {
+        isLoading = true
+        modelNode?.takeIf { !it.isAnchored }?.let {
+            sceneView.removeChild(it)
+            it.destroy()
+        }
+
+        modelNode = ArModelNode(model.placementMode).apply {
+            applyPoseRotation = model.applyPoseRotation
+            loadModelGlbAsync(
+                glbFileLocation = model.fileLocation,
+                autoAnimate = true,
+                scaleToUnits = model.scaleUnits,
+                // Place the model origin at the bottom center
+                centerOrigin = Position(y = -1.0f)
+            ) {
+                sceneView.planeRenderer.isVisible = true
+                isLoading = false
+            }
+            onAnchorChanged = { anchor ->
+                statusText.text = "onHitResult  placeModelButton.isGone = $anchor != null"
+
+            }
+            onHitResult = { node, _ ->
+                statusText.text = "onHitResult  placeModelButton.isGone = ${!node.isTracking}"
+            }
+        }
+        sceneView.addChild(modelNode!!)
+        // Select the model node by default (the model node is also selected on tap)
+        sceneView.selectedNode = modelNode
     }
 }
